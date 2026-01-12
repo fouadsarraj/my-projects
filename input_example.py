263cd0f351ecf7ba4c5f0919d430c9f8c2b0e56c
fouadsarraj <fivefifty299@gmail.com>
2026-01-12 20:49:35 +0300

    Create input_example.py

---
+++ input_example.py
@@ -0,0 +1,6 @@
+#This program calculates how many years until you graduate.
+
+current_year = 2025
+graduate_year = int(input(" Enter your graduation year: "))
+difference = graduation_year - current_year
+print( "Years until graduation :", difference)
